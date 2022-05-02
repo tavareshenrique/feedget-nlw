8a89af7e4c41eb8970eaa053832a20b94f574d3e
Henrique Tavares <ihenrits@gmail.com>
2022-05-02 18:50:38 -0300

    setup web project

---
+++ web/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx', '.ts'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
